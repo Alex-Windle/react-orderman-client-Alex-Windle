965337a926cf49a8b5174828647da5520e78c722
Mitch Masia <mjmasia@gmail.com>
2017-06-02 23:03:45 -0400

    Working base project

---
+++ app/boot.jsx
@@ -0,0 +1,35 @@
+/**
+ * GENERAL NOTES
+ * @author TalkRise <admin@talkrise.com>
+ *
+ * @description This is where your app kicks off
+ * This is where we take a base React component and connect it to the DOM
+ */
+
+
+// Module imports
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+/**
+ * Even though we don't import anything, this allows
+ * for the CSS files to be included in the project.
+ */
+import {} from './styles/cssImports';
+
+
+// Component imports
+import App from './views/App';
+
+
+/**
+ * We're saying that the <App /> should be mounted on the div with
+ * ID 'app'. If you look at our `index.ejs` file, you'll see that
+ * there is a simple div with the ID 'app'.
+ */
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
---
+++ app/views/Signup/SignupContainer.jsx
@@ -0,0 +1,30 @@
+/**
+ * GENERAL NOTES
+ * @author TalkRise <admin@talkrise.com>
+ */
+
+
+// Module imports
+import React, { Component } from 'react';
+
+
+/**
+ * SignupContainer represents the Signup page of the application.
+ *
+ * @description This pages is the connected container that renders:
+ *   SignupForm
+ *   Link to /login
+ *
+ * @extends Component
+ */
+export default class SignupContainer extends Component {
+  render() {
+    return <h1>You know what to do ;)</h1>;
+  }
+}
+
+
+SignupContainer.propTypes = {};
+
+
+SignupContainer.defaultProps = {};
