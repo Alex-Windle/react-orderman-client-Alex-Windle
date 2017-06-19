d406e0722f31d5856b5d934da3dec994a872b938
Alex Windle <AlexWind01@C02TF1FZG8WL.local>
2017-06-19 13:29:14 -0400

    Refactor logo (show image).

--- app/views/common/logos/Logo.jsx
+++ app/views/common/logos/Logo.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import logoImage from '../../../assets/logo.png';
 
-const Logo = () => (<div>LOGO</div>);
+const Logo = () => (
+  <img
+    src={logoImage}
+    alt="TalkRise OrderMan"
+    className="img"
+    width="150"
+  />
+);
 
 export default Logo;
